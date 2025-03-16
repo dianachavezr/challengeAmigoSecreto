16fc97bf847eb9facc0f5214186f93ec5d954a69
Diana Chavez R. <82002959+dianachavezr@users.noreply.github.com>
2025-03-16 14:53:07 -0500

    Add files via upload
    Se sube código del challenge Allura

---
+++ challenge-amigo-secreto_esp-main/app.js
@@ -0,0 +1,43 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+    
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+    
+    amigos.push(nombre);
+    input.value = "";
+    actualizarLista();
+}
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    
+    for (let amigo of amigos) {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Agrega al menos un amigo antes de sortear.");
+        return;
+    }
+    
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+    let resultadoLista = document.getElementById("resultado");
+    resultadoLista.innerHTML = "";
+    let li = document.createElement("li");
+    li.textContent = "El amigo secreto es: " + amigoSorteado;
+    resultadoLista.appendChild(li);
+}
